c56468512df7ad1061dc7e006bf8ebdbf2ab2f03
sscales-dev <scales.samantha@outlook.com>
2025-10-07 13:58:30 +0100

    Remove leading slash on form action attr. url

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 11 */
+
+function closeMenu () {
+    const toggleMenu = document.getElementById('nav-toggle-menu');
+
+    return toggleMenu.classList.remove('show');
+
+}
--- script.js
+++
@@ -1,6 +0,0 @@
-function closeMenu () {
-    const toggleMenu = document.getElementById('nav-toggle-menu')
-
-    return toggleMenu.classList.remove('show')
-
-}
